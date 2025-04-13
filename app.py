aff4ebde4db56944e45afa480554492e741f4fc3
yerkezhan-kz <tankaeva2012@mail.ru>
2025-04-13 20:24:59 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -17,4 +17,6 @@ def webhook():
     return "ok", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
